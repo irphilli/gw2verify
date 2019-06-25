05ec20b0a0cbcef7d610b0781271fbf55ef443fd
Phil Phillips <phil@unanimousai.com>
2019-06-25 16:29:09 -0700

    scaffolding for future commands

--- bot.rb
+++ bot.rb
@@ -4,6 +4,8 @@ require 'pp'
 require 'Faraday'
 
 API_ENDPOINT = 'https://api.guildwars2.com'
+ADMIN_CHANNEL = 'gw2verify-admin'
+VERIFICATION_CHANNEL = 'verification'
 
 @bot = Discordrb::Commands::CommandBot.new token: ENV['DISCORD_TOKEN'], prefix: '!'
 @redis = Redis.new
@@ -28,25 +30,83 @@ def load_world_info
   end
 end
 
-def add_account(account_id, key)
+def server_initialized?(server_id)
+  info = @redis.get("server_#{server_id}")
+  return !info.nil?
+end
+
+def add_account(server_id, account_id, key)
   # Check if key is valid
   response = Faraday.get "#{API_ENDPOINT}/v2/account?access_token=#{key}"
   raise "Couldn't retrieve account information. Check API key or try again later." if response.status != 200
   
   @redis.set("account_#{account_id}", key)
+  
+  # Set roles for current Discord server
+  
+  # Reset managed roles
+  
+  # If guild is set, set appropiate guild
+  
+  # Set role for server
 end
 
-# Here we output the invite URL to the console so the bot account can be invited to the channel. This only has to be
-# done once, afterwards, you can remove this part if you want
-puts "This bot's invite URL is #{@bot.invite_url}."
-puts 'Click on it to invite it to your server.'
+def reset_roles(server_id, account_id)
+  # If guild is set, reset guild role
+  
+  # Reset server roles
+end
 
 @bot.ready do |event|
   puts "Logged in as #{@bot.profile.username} (ID:#{@bot.profile.id}) | #{@bot.servers.size} servers"
 end
 
-@bot.command :key do |event, *args|
-  return if event.channel.nil? || event.channel.name != "verification"
+@bot.command :guild do |event, *args|
+  return if event.channel.nil? || event.channel.name != ADMIN_CHANNEL
+  
+  if args.length == 0
+    return
+  end
+  
+  # Adding a guild
+  # Make sure a role is set up for this guild
+  
+  # Set config
+  
+end
+
+@bot.command :servers do |event, *args|
+  return if event.channel.nil? || event.channel.name != ADMIN_CHANNEL
+  
+  if args.length == 0
+    return
+  end
+  
+  if args[0] == "add"
+    # Make sure role is set up for this server
+  elsif args[0] == "remove"
+  end
+  
+end
+
+@bot.command :refresh do |event, *args|
+  return if event.channel.nil? || event.channel.name != ADMIN_CHANNEL
+  
+  # For all users on discord server...
+  
+  # Remove managed roles
+  # Add guild role if part of guild
+  # Add applicable server role
+end
+
+@bot.command :verify do |event, *args|
+  return if event.channel.nil? || event.channel.name != VERIFICATION_CHANNEL
+  
+  if !server_initialized?(event.channel.server.id)
+    # TODO remove user's post
+    event.respond "Admin has not configured verify bot"
+    return
+  end
   
   if args.length != 1
     # TODO remove user's post
